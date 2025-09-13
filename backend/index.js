ac08e1d6898b77a8d4fbcf1f91cef06915c6169d
gauravkrsingh321 <gauravkrsingh40@gmail.com>
2025-09-13 10:29:52 +0530

    frontend url integrated

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://food-swipe-frontend.onrender.com",
     credentials: true
 }));
 app.use(cookieParser());
